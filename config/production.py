15af647f90d7847237c3d323517fa04ab6f62db2
austinsdoe <austinsdoe@gmail.com>
2025-04-25 09:32:01 +0000

    made changes to congig\production.py

--- config/production.py
+++ config/production.py
@@ -1,8 +1,8 @@
-from config.base import BaseConfig
+import os
 
-class ProdConfig(BaseConfig):
+class ProdConfig:
+    SECRET_KEY = os.environ.get("SECRET_KEY", "6db7fa6e05218ee246a9803e2237542d04dee5d1960f6a6e60d599db730a9e65")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "postgresql://austin:passSchollbo11@localhost/mikrotikdb")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
     SESSION_COOKIE_SECURE = True
-    
-    CAPTIVE_PORTAL_BASE_URL = os.getenv("CAPTIVE_PORTAL_BASE_URL", "http://http://127.0.0.1:5000//voucher/login")
-
